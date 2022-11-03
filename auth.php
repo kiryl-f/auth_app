b6be6e37fde1f6b19b7c12b75dee02671cc82e1e
kiryl-f <filippovickirill81@gmail.com>
2022-11-03 17:40:09 +0300

    auth page created

---
+++ auth.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <link rel="stylesheet" href="auth_style.css">
+    <title>Authentication</title>
+</head>
+
+<body>
+<h1 id="header">Enter your data here</h1>
+<form id="login">
+    <label for="lg">Login</label>
+    <input type="text" id="lg" placeholder="Enter your login here"><br>
+</form>
+<form id="password">
+    <label for="pw">Password</label>
+    <input type="password" id="pw"><br>
+</form>
+<form id="confirm_password">
+    <label for="c_pw">Confirm</label>
+    <input type="password" id="c_pw"><br>
+</form>
+<form id="email">
+    <label for="mail">Email</label>
+    <input type="email" id="mail" placeholder="email@example.com"><br>
+</form>
+<form id="name">
+    <label for="nm">Name</label>
+    <input type="text" id="nm" placeholder="Your name">
+</form>
+</body>
+</html>
