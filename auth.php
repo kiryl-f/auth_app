fef02f742460dd42a59ac9ec6086088eb53c8855
kiryl-f <filippovickirill81@gmail.com>
2022-11-07 22:17:04 +0300

    added some validation

--- auth.php
+++ auth.php
@@ -8,24 +8,46 @@
 <body>
 <h1 id="header">Enter your data here</h1>
 <form id="login">
+    <div class="alert " id="error" role="alert" style="display:none;">
+        <div><a href="#" class="close" data-dismiss="alert" aria-label="close">X</a></div>
+        <div class="error_show" id="error_show1"></div>
+    </div>
     <label for="lg">Login</label>
-    <input type="text" id="lg" placeholder="Enter your login here"><br>
+    <input type="text" id="lg" placeholder="Enter your login here" required><br>
 </form>
 <form id="password">
     <label for="pw">Password</label>
-    <input type="password" id="pw"><br>
+    <input type="password" id="pw" required><br>
 </form>
 <form id="confirm_password">
     <label for="c_pw">Confirm</label>
-    <input type="password" id="c_pw"><br>
+    <input type="password" id="c_pw" required><br>
 </form>
 <form id="email">
     <label for="mail">Email</label>
-    <input type="email" id="mail" placeholder="email@example.com"><br>
+    <input type="email" id="mail" placeholder="email@example.com" required><br>
 </form>
-<form id="name">
+<form name = "name_form" id="name_form"  >
     <label for="nm">Name</label>
-    <input type="text" id="nm" placeholder="Your name">
+    <input type="text" id="nm" name="nm" placeholder="Your name" required><br>
 </form>
+
+<form name="next" id="next" action="act.php" method="post" onsubmit="return validateForms()">
+    <input type="submit" value="Next">
+</form>
+
 </body>
 </html>
+
+<script>
+    function validateForms(){
+        let name = document.name_form.nm.value.trim();
+        if (name==null || name==="") {
+            alert("Name can't be blank");
+            return false;
+        }
+    }
+
+</script>
+
+<script src="js/fetch.js"></script>
---
+++ js/fetch.js
@@ -0,0 +1,27 @@
+/*
+$(document).ready(function() {
+    $('#butsave').on('click', function() {
+        var login = $('#login').val();
+        if(login === "") {
+            $("#error").show();
+            $('#error_show1').html('Please fill all the field!');
+        }
+    });
+}*/
+
+/*
+function next() {
+// Get the snackbar DIV
+    var x = document.getElementById("a");
+
+    // Add the "show" class to DIV
+    x.className = "show";
+    var login = $('#login').val();
+    x.textContent = login;
+    // After 3 seconds, remove the show class from DIV
+    if(login === "aaa") {
+        setTimeout(function(){ x.className = x.className.replace("show", ""); }, 3000);
+    }
+
+}*/
+
