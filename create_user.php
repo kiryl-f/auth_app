af102c7adc4ce088da12c4985f03cb729a735cb4
kiryl-f <filippovickirill81@gmail.com>
2022-11-13 21:29:54 +0300

    xml db added

--- create_user.php
+++ create_user.php
@@ -2,9 +2,10 @@
 
 $xml = simplexml_load_file("users.xml");
 $users = new SimpleXMLElement($xml->asXML());
+$size = $xml->count();
 $users->addChild("user");
-$users->addChild("login", $_POST['login']);
-$users->addChild("password", $_POST['password']);
-$users->addChild("email", $_POST['email']);
-$users->addChild("name", $_POST['name']);
+$users->user[$size]->addChild("login", $_POST['login']);
+$users->user[$size]->addChild("password", $_POST['password']);
+$users->user[$size]->addChild("email", $_POST['email']);
+$users->user[$size]->addChild("name", $_POST['name']);
 $users->asXML("users.xml");
