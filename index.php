51383d2939afefb4db04dec674ec5482c259b8cb
kiryl-f <67046881+kiryl-f@users.noreply.github.com>
2022-11-14 23:27:28 +0300

    Update index.php

--- index.php
+++ index.php
@@ -2,23 +2,26 @@
 <html lang="en">
 <head>
     <title>Main page!</title>
+    <link rel="stylesheet" href="auth_style.css">
 </head>
 <body>
     <?php
         require_once 'is_set.php';
-        $style = "";
+        $log_out_style = "";
+        $log_in_style = "";
         if(user_remembered()) {
             echo "Hello, " . $_COOKIE['name'] . "<br>";
+            $log_in_style = "style='display:none;'";
         } else {
-            $style = "style='display:none;'";
+            $log_out_style = "style='display:none;'";
         }
 
     ?>
-    <a href="auth.php">Login</a>
+    <a href="auth.php" <?php echo $log_in_style?>>Login</a>
     <br>
     <a href="registration.php">Create an account</a>
     <br>
-    <form action="log_out.php" method="post" <?php echo $style;?>>
+    <form action="log_out.php" method="post" <?php echo $log_out_style;?>>
         <button type="submit">Log out</button>
     </form>
 
