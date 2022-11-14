9d4ce801ae418d487844c33c9c2147b1f02918d0
kiryl-f <filippovickirill81@gmail.com>
2022-11-14 23:31:45 +0300

    final version

---
+++ reg_ajax.js
@@ -0,0 +1,29 @@
+$(function () {
+    $('form').on('submit', function (e) {
+        e.preventDefault();
+
+        var password = $('#password').val();
+        var confirm_password = $('#confirm_password').val();
+        if(password === confirm_password) {
+            $.ajax({
+                type: 'post',
+                url: 'create_user.php',
+                data: $('form').serialize(),
+                success: function (response) {
+                    if(response === 'login'){
+                        alert('This login is taken');
+                    } else if(response === 'email') {
+                        alert('This email is taken');
+                    } else {
+                        location.href = 'index.php';
+                    }
+                },
+                error: function (jqXHR, textStatus, errorThrown) {
+                    alert(errorThrown);
+                }
+            });
+        } else {
+            alert('Passwords do not match');
+        }
+    });
+});
