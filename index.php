0d1b5f1c8b433b9c6ec8e8ba482401004391926c
kiryl-f <67046881+kiryl-f@users.noreply.github.com>
2022-11-15 10:48:43 +0300

    Update index.php

--- index.php
+++ index.php
@@ -9,9 +9,11 @@
         require_once 'is_set.php';
         $log_out_style = "";
         $log_in_style = "";
+        $create_user_style = "";
         if(user_remembered()) {
             echo "Hello, " . $_COOKIE['name'] . "<br>";
             $log_in_style = "style='display:none;'";
+            $create_user_style = "style='display:none;'";
         } else {
             $log_out_style = "style='display:none;'";
         }
@@ -19,7 +21,7 @@
     ?>
     <a href="auth.php" <?php echo $log_in_style?>>Login</a>
     <br>
-    <a href="registration.php">Create an account</a>
+    <a href="registration.php" <?php echo $create_user_style?>>Create an account</a>
     <br>
     <form action="log_out.php" method="post" <?php echo $log_out_style;?>>
         <button type="submit">Log out</button>
